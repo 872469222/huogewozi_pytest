8fe50c3a24463c0a9e7780a6378869195853749a
liaoboyang <2602161142@qq.com>
2023-05-17 18:40:35 +0800

    pytest 命令参数/xfail/参数化/Python代码执行pytest(3种方式)/常用的异常处理方法

---
+++ demo1/demo4/test_raise.py
@@ -0,0 +1,13 @@
+import pytest
+#sasda
+
+def test_raise():
+    with pytest.raises((ZeroDivisionError,ValueError)):
+        raise ValueError("除数为0")
+
+
+def test_raise1():
+    with pytest.raises(ValueError) as exc_info:
+        raise ValueError("value must be 42")
+    assert exc_info.type is ValueError
+    assert exc_info.value.args[0] == "value must be 42"
